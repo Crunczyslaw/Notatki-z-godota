90773ded0130b096a77138117169a30c1ed28042
Crunczysław <gabriel.szturmowiec@gmail.com>
2026-07-08 12:21:45 +0200

    Delete Notatki/Pierwsze Postępy w edytorze/MalyProjektZPilkami directory

--- Notatki/Pierwsze Postępy w edytorze/MalyProjektZPilkami/Main.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-@export var Ball: PackedScene
-
-func _input(event):
-	if event.is_action_pressed("click"):
-		var new_ball = Ball.instantiate() # instantiate() robi dosłownie kopie 
-		new_ball.position = get_viewport().get_mouse_position() # bierze pozycje myszki względem pozycji na ekranie (nie pozycji growej)
-		add_child(new_ball)
--- Notatki/Pierwsze Postępy w edytorze/MalyProjektZPilkami/README.md
+++
@@ -1,3 +0,0 @@
-# Zrobiony na podstawie tutoriala
-
-https://docs.godotengine.org/en/latest/getting_started/step_by_step/index.html
